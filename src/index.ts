451acad190ee6eb99b96f319c308107a5af9cbd9
james <jetter019@msn.com>
2018-01-02 22:36:10 +0100

    Using some different technology because OS bonjour is a total failure.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 
+import * as bonjourModule from "bonjour";
+const bonjour = bonjourModule();
+
+// advertise an HTTP server on port 3000 
+bonjour.publish({ name: 'My Web Server', type: 'http', port: 3000 });
+ 
+// browse for all http services 
+bonjour.find({ type: 'http' }, function (service) {
+  console.log('Found an HTTP server:', service)
+});

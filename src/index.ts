707ca509ed9ca265cc870896d7da77474ad2b324
james <jetter019@msn.com>
2018-01-03 00:42:56 +0100

    Made this great again.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-
+import * as http from "http";
 import * as bonjourModule from "bonjour";
+import * as request from "request";
 const bonjour = bonjourModule();
+
+const server = http.createServer((req, res) => {
+    res.write('Send this to the client');
+    res.end();
+});
+server.listen(3000);
 
 // advertise an HTTP server on port 3000 
 bonjour.publish({ name: 'My Web Server', type: 'http', port: 3000 });
- 
+
 // browse for all http services 
-bonjour.find({ type: 'http' }, function (service) {
-  console.log('Found an HTTP server:', service)
+bonjour.find({ type: 'http' }, function (service: bonjourModule.Service) {
+    console.log('Found an HTTP server:', service);
+    doAction(service.referer.address, service.port);
 });
+
+function doAction(address: string, port: number){
+    console.log('What did the address end up being: ' + address);
+    console.log('and the port is: ' + port);
+    const req = request(`http://${address}:${port}`, (error, response: request.RequestResponse, body) => {
+        console.log('What did i get back ' + response);
+        console.log('what is the body: ' + body);
+    });
+}

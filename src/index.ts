a9cf81731b72f0b25319f76bdb5c00b838623275
james <jetter019@msn.com>
2018-01-03 04:25:17 +0100

    Added some tslint goodness.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,30 @@
-import * as http from "http";
 import * as bonjourModule from "bonjour";
+import * as http from "http";
 import * as request from "request";
 const bonjour = bonjourModule();
 
+const PORT = 3000;
+
 const server = http.createServer((req, res) => {
-    res.write('Send this to the client');
+    res.write("Send this to the client");
     res.end();
 });
 server.listen(3000);
 
-// advertise an HTTP server on port 3000 
-bonjour.publish({ name: 'My Web Server', type: 'http', port: 3000 });
+// advertise an HTTP server on port 3000
+bonjour.publish({ name: "My Web Server", type: "http", port: 3000 });
 
-// browse for all http services 
-bonjour.find({ type: 'http' }, function (service: bonjourModule.Service) {
-    console.log('Found an HTTP server:', service);
+// browse for all http services
+bonjour.find({ type: "http" }, (service: bonjourModule.Service) => {
+    console.log("Found an HTTP server:", service);
     doAction(service.referer.address, service.port);
 });
 
-function doAction(address: string, port: number){
-    console.log('What did the address end up being: ' + address);
-    console.log('and the port is: ' + port);
+function doAction(address: string, port: number) {
+    console.log("What did the address end up being: " + address);
+    console.log("and the port is: " + port);
     const req = request(`http://${address}:${port}`, (error, response: request.RequestResponse, body) => {
-        console.log('What did i get back ' + response);
-        console.log('what is the body: ' + body);
+        console.log("What did i get back " + response);
+        console.log("what is the body: " + body);
     });
 }

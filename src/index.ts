c2e6d2c558b8f06a3dbe44df5e207d64ac929e8d
james <jetter019@msn.com>
2018-01-03 05:10:19 +0100

    Made this slightly greater again.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as bonjourModule from "bonjour";
 import * as http from "http";
-import * as request from "request";
 const bonjour = bonjourModule();
 
 const PORT = 3000;
@@ -8,23 +7,12 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
     res.write("Send this to the client");
     res.end();
-});
-server.listen(3000);
-
-// advertise an HTTP server on port 3000
-bonjour.publish({ name: "My Web Server", type: "http", port: 3000 });
-
-// browse for all http services
-bonjour.find({ type: "http" }, (service: bonjourModule.Service) => {
-    console.log("Found an HTTP server:", service);
-    doAction(service.referer.address, service.port);
-});
-
-function doAction(address: string, port: number) {
-    console.log("What did the address end up being: " + address);
-    console.log("and the port is: " + port);
-    const req = request(`http://${address}:${port}`, (error, response: request.RequestResponse, body) => {
-        console.log("What did i get back " + response);
-        console.log("what is the body: " + body);
+    res.on('finish', () => {
+        server.close();
+        bonjour.destroy();
+        process.exit(0);
     });
-}
+});
+server.listen(PORT);
+// advertise an HTTP server on port 3000
+bonjour.publish({ name: "Music Voting", type: "music-voting", port: PORT });
---
+++ test/test.ts
@@ -0,0 +1,20 @@
+import * as bonjourModule from "bonjour";
+import * as http from "http";
+import * as request from "request";
+const bonjour = bonjourModule();
+
+// browse for all http services
+bonjour.find({ type: "music-voting" }, (service: bonjourModule.Service) => {
+    console.log("Found an HTTP server:", service);
+    doAction(service.referer.address, service.port);
+});
+
+function doAction(address: string, port: number) {
+    console.log("What did the address end up being: " + address);
+    console.log("and the port is: " + port);
+    const req = request(`http://${address}:${port}`, (error: any, response: request.RequestResponse, body: any) => {
+        console.log("What did i get back " + response);
+        console.log("what is the body: " + body);
+        process.exit(0);
+    });
+}

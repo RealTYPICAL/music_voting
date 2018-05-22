70812e9a95521d8ee1ca22948321d919ee3a7c51
james <jetter019@msn.com>
2018-05-22 15:28:39 +0200

    Finally discoverable from android.

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,27 @@ import * as express from "express";
 import * as http from "http";
 import { extendApi } from "./votingApi";
 
+// import the module
+var mdns = require('mdns');
+ 
+// advertise a http server on port 4321
+var ad = mdns.createAdvertisement(mdns.tcp('http'), 4321, { name: "secret pronz channel" });
+ad.start();
+ 
+// watch all http servers
+var browser = mdns.createBrowser(mdns.tcp('http'));
+browser.on('serviceUp', function(service: any) {
+  console.log("service up: ", service);
+});
+browser.on('serviceDown', function(service: any) {
+  console.log("service down: ", service);
+});
+browser.start();
+
 const app = express();
 const bonjour = bonjourModule({ multicast: true });
 
-const PORT = 3000;
+const PORT = 3005;
 
 app.use(express.json());       // to support JSON-encoded bodies
 // app.use(express.urlencoded()); // to support URL-encoded bodies
@@ -15,6 +32,6 @@ extendApi(app);
 
 app.listen(PORT);
 
-bonjour.publish({ name: "music", type: "http", port: PORT });
+bonjour.publish({ name: "musicsomething", type: "http", port: PORT }).start();
 
 console.log("Now setup.");
